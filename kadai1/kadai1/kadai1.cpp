74d1d4735c4fc19d0ee3fada80fe4d4957d9da1a
Yuya-1130 <ktc25a31a0059@edu.kyoto-tech.ac.jp>
2026-07-18 16:29:10 +0900

    未完成

---
+++ kadai1/kadai1/kadai1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	// int型のコンテナを宣言し1～10までの整数値を追加する
+	std::vector<int> nums = { 1,2,3,4,5,6,7,8,9,10 };
+
+	// コンテナから偶数だけを削除する
+
+	for (std::vector<int>::iterator it = nums.begin(); it != nums.end();) 
+	{
+
+		//std::vector<int>::iterator position = nums.begin();
+		
+	
+
+		it = nums.erase(it);
+
+		
+	}
+	
+}
+
+
