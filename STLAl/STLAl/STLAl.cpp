1bde83c1de643dd03a42f5e8b63029d27e12ccf6
Yuya-1130 <ktc25a31a0059@edu.kyoto-tech.ac.jp>
2026-07-18 15:20:28 +0900

    アルゴリズム

---
+++ STLAl/STLAl/STLAl.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// 二つの値を受け取って第一のほうが大きい場合に true を返す
+bool greater(int l,int r) {
+	return l > r;
+}
+
+
+// エントリー関数
+int main()
+{
+	std::vector<int> vec = { 10,8,4,9,2,3,5 };
+
+	std::sort(vec.begin(), vec.end(),greater);
+	// 小さい値空大きい値の順に並び変える
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+}
+
